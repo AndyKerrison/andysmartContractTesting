fd6d55356eabc89d4d188f3f91dc21fae351f009
Andrew Kerrison <adesignforlife@gmail.com>
2018-05-29 08:57:31 +0100

    Contract updates, min/max pledges etc

--- CurvePoolContract.sol
+++ CurvePoolContract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.21;
 
 // ERC20 Interface: https://github.com/ethereum/EIPs/issues/20
 contract ERC20 {
@@ -12,6 +12,7 @@ contract CurvePoolContract{
     address public _owner;
  
     mapping (address => uint256) public _etherDeposits;
+    mapping (address => uint256) public _pledges;
     mapping (address => bool) public _isEthWithdrawn;
     mapping (address => bool) public _isTokensWithdrawn;
     
@@ -23,26 +24,48 @@ contract CurvePoolContract{
     
     bool public _depositsLocked;
     bool public _tokensReceived;
-    bool public _contractOpen;
     bool public _pledgesEnabled;
     
     uint256 public _pledgesEndDate;
     
     uint256 public _transactionFee;
     
+    uint256 public _minContribution;
+    uint256 public _maxContribution;
+    
     uint256 public _totalEthContributed;
     uint256 public _totalEthUnspent;
     uint256 public _totalTokenBalance;
     
-    function CurvePoolContract() public
+    constructor(address sale, address token, uint256 transactionFee, uint256 minContribution, uint256 maxContribution, uint256 pledgesEndDate, bool pledgesEnabled) public
     {
         _owner= msg.sender;  
+        
+        _sale = sale;
+        _token = ERC20(token);
+        _transactionFee = transactionFee;
+        _minContribution = minContribution;
+        _maxContribution = maxContribution;
+        _pledgesEndDate = pledgesEndDate;
+        _pledgesEnabled = pledgesEnabled;
     }
     
     modifier onlyOwner {
         require (msg.sender==_owner);
         _;
     }
+    
+    function setSaleAddress(address sale) public onlyOwner {
+        //only allow this before pool is locked
+        require(!_depositsLocked);
+        _sale = sale;
+    } 
+    
+    //set the token address
+    function setTokenAddress(address token) public onlyOwner {
+        require(!_tokensReceived);
+        _token = ERC20(token);
+    } 
     
     function getUserTokenBalance(address userAddress) constant public returns(uint256) 
     {
@@ -72,43 +95,38 @@ contract CurvePoolContract{
         
         return (ethContributed*_totalEthUnspent)/_totalEthContributed;
     }
-       
-    //set the sale address
-    function setTransactionFee(uint256 transactionFee) public onlyOwner {
-        if (transactionFee < 0)
-            revert();
-        
-        _transactionFee = transactionFee;
-    } 
-    
-    function setContractOpen() public onlyOwner {
-        _contractOpen = true;
-    } 
     
     function setDepositsLocked(bool locked) public onlyOwner {
         _depositsLocked = locked;
     }
     
-    function setPledgesEnabled(bool locked) public onlyOwner {
-        _pledgesEnabled = locked;
+    /*   
+    function setTransactionFee(uint256 transactionFee) public onlyOwner {
+        require(transactionFee >= 0);  
+        _transactionFee = transactionFee;
+    } 
+    
+    function setPledgesEnabled(bool enabled) public onlyOwner {
+        _pledgesEnabled = enabled;
     } 
     
     function setPledgesEndDate(uint256 pledgesEndDate) public onlyOwner {
         _pledgesEndDate = pledgesEndDate;
+    }
+    
+    function setMinContribution(uint256 minVal) public onlyOwner {
+        _minContribution = minVal;
     } 
     
-    function setSaleAddress(address sale) public onlyOwner {
-        _sale = sale;
+    function setMaxContribution(uint256 maxVal) public onlyOwner {
+        _maxContribution = maxVal;
     } 
-    
-    //set the token address
-    function setTokenAddress(address token) public onlyOwner {
-        _token = ERC20(token);
-    } 
-    
+    */
+
     //in case something goes wrong and we need to recover funds
     function emergencyEtherWithdraw() public onlyOwner {
-        _owner.transfer(this.balance);
+        address myAddress = this;
+        _owner.transfer(myAddress.balance);
     }
     
     //in case something goes wrong and we need to recover funds
@@ -118,13 +136,13 @@ contract CurvePoolContract{
     
    
     function buyTokens() onlyOwner public returns(uint) {
-        //prerequisites. Return instead of throwing in order to save gas
-        if (_sale == 0x0) return 2; //must have a sale address
+	
+        //prerequisites.
+		if (_sale == 0x0) return 2; //must have a sale address
         
         if (_tokensReceived) return 3; //only retieve tokens once
         
         //in the future we may split into multiple purchases
-        //LockDeposits(); 
         _depositsLocked = true;
         
         uint256 ethToSpend;
@@ -157,7 +175,8 @@ contract CurvePoolContract{
     
     function setTokensReceived() public onlyOwner
     {
-        _totalEthUnspent = this.balance;
+        address myAddress = this;
+        _totalEthUnspent = myAddress.balance;
         _tokensReceived = true;
         
         if (_token != ERC20(0x0))
@@ -177,14 +196,24 @@ contract CurvePoolContract{
     
     function depositEther() internal
     {
-        require(_contractOpen && !_depositsLocked && !_tokensReceived);
+        require(!_depositsLocked && !_tokensReceived);
+        
+        //cannot deposit until pledge phase complete.
+        require(!_pledgesEnabled || now > _pledgesEndDate);
+
+        //cannot donate more than max
+        require(_maxContribution == 0 || _etherDeposits[msg.sender] + msg.value <= _maxContribution);
             
+        //cannot donate more than pledge
+        require(!_pledgesEnabled || _etherDeposits[msg.sender] + msg.value <= _pledges[msg.sender]);
+        
         _etherDeposits[msg.sender] += msg.value; //use SAFEADD?
         _addressArray.push(msg.sender);
         _totalAddresses++;
             
-        _totalEthContributed = this.balance;
-        _totalEthUnspent = this.balance;
+        address myAddress = this;
+        _totalEthContributed = myAddress.balance;
+        _totalEthUnspent = myAddress.balance;
     }
     
     function safeTokenTransfer(address target, uint256 value) internal returns(bool)
@@ -205,15 +234,27 @@ contract CurvePoolContract{
         }
     }
     
+    function setPledgeValue(uint256 amount) public
+    {
+        require(!_tokensReceived && !_depositsLocked);
+        require(_pledgesEnabled && now <= _pledgesEndDate);
+        require(amount <= _maxContribution || _maxContribution == 0);
+        require(amount >= _minContribution);
+        
+        _pledges[msg.sender] = amount;
+    }
+    
     //can only call this one before tokens have been purchased
     function withdrawEth(uint256 amount) public 
     {
-        require(_contractOpen && !_tokensReceived && !_depositsLocked);
+        require(!_tokensReceived && !_depositsLocked);
         
         //compare to how much eth the user put in
         uint256 ethContributed = _etherDeposits[msg.sender];
         
         require(amount <= ethContributed && amount > 0);
+        //cannot set contribution less than min, unless zero
+        require(_minContribution == 0 || ethContributed - amount >= _minContribution);
         assert(amount <= this.balance);
         
         _etherDeposits[msg.sender] -= amount;
@@ -224,7 +265,7 @@ contract CurvePoolContract{
     //withdraw all funds. Call only AFTER tokens are done
     function withdrawFunds() public 
     {
-        require(_contractOpen && !_depositsLocked);
+        require(!_depositsLocked);
         
         uint256 ethOwed = getUserEtherBalance(msg.sender);
         uint256 userTokens = getUserTokenBalance(msg.sender);
